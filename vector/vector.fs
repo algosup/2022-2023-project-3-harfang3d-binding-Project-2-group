d70d84d24601349f0a47913eab23cea6da14b396
audreytllz <114394252+audreytllz@users.noreply.github.com>
2023-01-25 14:58:23 +0100

    update

--- vector/vector.fs
+++ vector/vector.fs
@@ -1,30 +1,62 @@
-type Vector2(x:float, y:float) =
-    member this.X = x
-    member this.Y = y
-    member this.DistanceTo(pos:Vector2) = 
-        sqrt( (pos.Y - this.Y) * (pos.Y - this.Y) + (pos.X - this.X) * (pos.X - this.X))
-    member this.VectorMovement(plusx:float, plusy:float) = 
-        this.X <- this.X + plusx
-        this.Y <- this.Y + plusy
-    member this.Midpoint(pos:Vector2) = 
-        let mx = (this.X + pos.X) / 2.0
-        let my = (this.Y + pos.Y) / 2.0
-        Vector2(mx, my)
-    member this.PercentDistance(pos:Vector2, percentOfDistance:float) = 
-        this.DistanceTo(pos) / (100.0 / percentOfDistance)
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <math.h>
 
-type Vector3(x:float, y:float, z:float) =
-    inherit Vector2(x, y)
-    member this.Z = z
-    override this.DistanceTo(pos:Vector3) = 
-        sqrt( (pos.Y - this.Y) * (pos.Y - this.Y) + (pos.X - this.X) * (pos.X - this.X) + (pos.Z - this.Z) * (pos.Z - this.Z))
-    override this.VectorMovement(plusx:float, plusy:float, plusz:float) = 
-        base.VectorMovement(plusx, plusy)
-        this.Z <- this.Z + plusz
-    override this.Midpoint(pos:Vector3) = 
-        let mx = (this.X + pos.X) / 2.0
-        let my = (this.Y + pos.Y) / 2.0
-        let mz = (this.Z + pos.Z) / 2.0
-        Vector3(mx, my, mz)
-    override this.PercentDistance(pos:Vector3, percentOfDistance:float) = 
-        base.PercentDistance(pos, percentOfDistance)
+class Vector2 {
+public:
+    Vector2(double inx = 0, double iny = 0) {
+        x = inx;
+        y = iny;
+    }
+    double x;
+    double y;
+    double distanceTo(Vector2 pos) {
+        return sqrt((pos.y - y) * (pos.y - y) + (pos.x - x) * (pos.x - x));
+    }
+    void vectorMovement(double plusx, double plusy) {
+        x += plusx;
+        y += plusy;
+        return;
+    }
+    Vector2 midpoint(Vector2 pos) {
+        double mx = (x + pos.x) / 2;
+        double my = (y + pos.y) / 2;
+        Vector2 mid(mx, my);
+        return mid;
+    }
+    double percentDistance(Vector2 pos, double percentOfDistance = 100) {
+        return distanceTo(pos) / (100 / percentOfDistance);
+    }
+};
+class Vector3 {
+public:
+    Vector3(double inx = 0, double iny = 0, double inz = 0) {
+        x = inx;
+        y = iny;
+        z = inz;
+    }
+    double x;
+    double y;
+    double z;
+    double distanceTo(Vector3 pos) {
+        return sqrt((pos.y - y) * (pos.y - y) + (pos.x - x) * (pos.x - x) + (pos.z - z) * (pos.z - z));
+    }
+    void vectorMovement(double plusx, double plusy, double plusz) {
+        x += plusx;
+        y += plusy;
+        z += plusz;
+        return;
+    }
+    Vector3 midpoint(Vector3 pos) {
+        double mx = (x + pos.x) / 2;
+        double my = (y + pos.y) / 2;
+        double mz = (z + pos.z) / 2;
+        Vector3 mid(mx, my, mz);
+        return mid;
+    }
+    double percentDistance(Vector3 pos, double percentOfDistance = 100) {
+        return distanceTo(pos) / (100 / percentOfDistance);
+    }
+};
