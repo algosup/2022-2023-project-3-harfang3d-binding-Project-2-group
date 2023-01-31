dc84eb55866f7d14c5d34343c87bd30d603f142c
Pierre Gorin <pierre.gorin@algosup.com>
2023-01-31 15:08:27 +0100

    can use shared library

---
+++ vector/fSharpCode/Program.fs
@@ -0,0 +1,13 @@
+﻿open System.Runtime.InteropServices
+
+[<DllImport("libVectorsSharedLibraries.dylib")>]
+extern double distanceTo(double x1, double y1, double x2, double y2);
+
+let x1: float = 5.0
+let y1: float = 1.0
+let x2: float = 2.0
+let y2: float = 2.0
+
+let distance: double = distanceTo(x1, y1, x2, y2)
+
+printfn "Distance between (%f, %f) and (%f, %f) is %f" x1 y1 x2 y2 distance
