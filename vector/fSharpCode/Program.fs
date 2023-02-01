094e60d1ac28bef9886fc496a2d78b099647035e
Alexandre <alexandre.bobis@algosup.com>
2023-02-01 12:17:49 +0100

    Update Mac test

--- vector/fSharpCode/Program.fs
+++ vector/fSharpCode/Program.fs
@@ -1,6 +1,7 @@
 ﻿open System.Runtime.InteropServices
 
-[<DllImport("VectorsSharedLibraries.dll")>]
+[<DllImport("VectorsSharedLibraries.dylib")>] // Mac
+// [<DllImport("VectorsSharedLibraries.dll")>] // Windows
 extern double distanceTo(double x1, double y1, double x2, double y2);
 
 let x1: float = 5.0
@@ -11,3 +12,15 @@ let y2: float = 2.0
 let distance: double = distanceTo(x1, y1, x2, y2)
 
 printfn "Distance between (%f, %f) and (%f, %f) is %f" x1 y1 x2 y2 distance
+
+[<DllImport("VectorsSharedLibraries.dylib")>] // Mac
+
+extern double v3distanceTo(double x3, double y3, double z3);
+
+let x3: float = 5.0
+let y3: float = 6.0
+let z3: float = 3.0
+
+let v3distance: double = v3distanceTo(x3, y3, z3)
+
+printfn "Distance between (%f, %f, %f) and (0, 0, 0) is %f" x3 y3 z3 v3distance
