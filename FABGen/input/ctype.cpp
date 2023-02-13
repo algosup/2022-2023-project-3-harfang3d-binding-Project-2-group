b0ddc3069be67d7c46a7c8247b48629eb0c512b1
Salaheddine Namir <71770514+T3rryc@users.noreply.github.com>
2023-02-13 12:20:21 +0100

    math, assert, ctype and string lib
    setup for test more cmake dll dylib build

---
+++ FABGen/input/ctype.cpp
@@ -0,0 +1,17 @@
+/* isalnum example */
+#include <stdio.h>
+#include <ctype.h>
+
+#ifdef _WIN32
+__declspec(dllexport)
+#endif
+
+int main ()
+{
+  int i;
+  char str[]="c3po...";
+  i=0;
+  while (isalnum(str[i])) i++;
+  printf ("The first %d characters are alphanumeric.\n",i);
+  return 0;
+}
---
+++ FABGen/input/string.cpp
@@ -0,0 +1,18 @@
+/* strcpy example */
+#include <stdio.h>
+#include <string.h>
+
+#ifdef _WIN32
+__declspec(dllexport)
+#endif
+
+int main ()
+{
+  char str1[]="Sample string";
+  char str2[40];
+  char str3[40];
+  strcpy (str2,str1);
+  strcpy (str3,"copy successful");
+  printf ("str1: %s\nstr2: %s\nstr3: %s\n",str1,str2,str3);
+  return 0;
+}
